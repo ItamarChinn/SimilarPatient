41b89388932b964904d1969b2800014a9dfc547d
itamarchinn <itamar@chinn.com>
2021-02-07 17:27:32 +0200

    Added beginning of python communication

--- fhir-kit-create-react-master/server/main.js
+++ fhir-kit-create-react-master/server/main.js
@@ -21,16 +21,23 @@ app.get('/api/patient', (req, res) => {
     // })
     fhirClient.request('Patient')
     .then((response) => {
-      const patients = response.entry ? response.entry.map((obj) => {
-        return {
-          id: obj.resource.id,
-          name: `${obj.resource.name[0].given} ${obj.resource.name[0].family}`,
-          gender: obj.resource.gender,
-          birthDate: obj.resource.birthDate
-        }
-      }) : [];
+      // https://stackoverflow.com/questions/23450534/how-to-call-a-python-function-from-node-js
+      const spawn = require("child_process").spawn;
+      const pythonProcess = spawn('python',["model.py", response.entry]);
+      pythonProcess.stdout.on('data', (data) => {
+        console.log(data.toString());
+      });
 
-      res.status(200).json(patients);
+      // const patients = response.entry ? response.entry.map((obj) => {
+      //   return {
+      //     id: obj.resource.id,
+      //     name: `${obj.resource.name[0].given} ${obj.resource.name[0].family}`,
+      //     gender: obj.resource.gender,
+      //     birthDate: obj.resource.birthDate
+      //   }
+      // }) : [];
+
+      // res.status(200).json(patients);
     });
   } else {
     res.status(200).json([]);
