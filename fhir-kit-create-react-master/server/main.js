c0c5e35e4318f07d0f791389ab385f23f0925a5e
Itamar Chinn <itamarc@mit.edu>
2021-03-07 15:21:32 +0200

    Fixing a major error

--- fhir-kit-create-react-master/server/main.js
+++ fhir-kit-create-react-master/server/main.js
@@ -1,96 +1,88 @@
-import React, { useState } from 'react';
-import { Layout, Input, List, Card, Row, Col, Spin } from 'antd';
-import Patient from './Patient';
-import './App.css';
+const express = require('express');
+const Client = require('fhir-kit-client');
 
-const { Header, Content } = Layout;
-const Search = Input.Search;
+const config = { baseUrl: 'https://r3.smarthealthit.org/' };
+const fhirClient = new Client(config);
+  
 
-const App = () => {
-  const [ loading, setLoading ] = useState(false);
-  const [ patients, setPatients ] = useState([]);
-  const [ searchResolved, setSearchResolved ] = useState(false);
+const app = express();
 
-  const searchPatientNames = (name) => {
-    setLoading(true);
-    setSearchResolved(false);
+app.set("port", process.env.PORT || 3001);
 
-    fetch(`api/patient?name=${name}`, {
-      accept: 'application/json'
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+}
+
+app.get('/api/patient', (req, res) => {
+  if (req.query.name) {
+    client.search({
+      resourceType: 'Patient',
+      searchParams: { name: req.query.name }
     })
-    .then((response) => (
-      response.json()
-    ))
-    .then((patients) => {
-      setPatients(patients || []);
-      setLoading(false);
-      setSearchResolved(true);
-    })
-    .catch((err) => {
-      console.log(err);
-      setLoading(false);
-    })
+    .then((response) => {
+      const patients = response.entry ? response.entry.map((obj) => {
+        return {
+          id: obj.resource.id,
+          name: `${obj.resource.name[0].given} ${obj.resource.name[0].family}`,
+          gender: obj.resource.gender,
+          birthDate: obj.resource.birthDate
+        }
+      }) : [];
+
+      res.status(200).json(patients);
+    });
+  } else {
+    res.status(200).json([]);
   }
+});
 
-  return (
-    <Layout className="App">
-      <Header className="App-header">
-        <h1>FHIR Kit: Create React App</h1>
-      </Header>
-      <Content className="App-content">
-        <Row>
-          <Col span={10} offset={7}>
-            <p>This is an example React app generated with the
-              <a
-                href="https://github.com/Vermonster/fhir-kit-create-react"
-                rel="noopener noreferrer"
-                target="_blank"
-                title="FHIRKit Create React App repo"> FHIRKit Create React App</a>
-              template. The generated Node.js backend uses
-              <a
-                href="https://github.com/Vermonster/fhir-kit-client"
-                rel="noopener noreferrer"
-                target="_blank"
-                title="FHIRKit Client repo">FHIRKit Client</a>
-              to search an open FHIR server.</p>
-          </Col>
-        </Row>
-        <h2>Patient Name Search Example</h2>
-        <Search
-          className="App-search"
-          placeholder="Search Patient Names"
-          enterButton="Search"
-          size="large"
-          onSearch={searchPatientNames}
-        />
-        { loading ? (
-            <Row type="flex" justify="center">
-              <Col span={4}>
-                <Spin/>
-              </Col>
-            </Row>
-          ) : (
-            <List
-              className="App-list"
-              grid={{ gutter: 16, column: 2 }}
-              dataSource={patients}
-              locale={searchResolved ? { emptyText: 'No results found.' } : { emptyText: '' }}
-              renderItem={patient => (
-                <List.Item>
-                  <Card title={patient.name}>
-                  <Patient
-                    id={patient.id}
-                    name={patient.name}
-                    birthDate={patient.birthDate}
-                    gender={patient.gender} />
-                  </Card>
-                </List.Item>
-              )}
-            />
-        ) }
-      </Content>
-    </Layout>
-  );
-};
+app.get('/api/similarpatient', (req, res) => {
+  if (req.query.name) {
+    // client.search({
+    //   resourceType: 'Patient',
+    //   searchParams: { name: req.query.name }
+    // })
+    fhirClient.request('Patient')
+    .then((response) => {
 
-export default App;
+      const patients = response.entry ? response.entry.map((obj) => {
+        return {
+          id: obj.resource.id,
+          name: `${obj.resource.name[0].given} ${obj.resource.name[0].family}`,
+          gender: obj.resource.gender,
+          birthDate: obj.resource.birthDate
+        }
+      }) : [];
+
+      const patient_list = response.entry ? response.entry.map((obj) => {
+        return obj.resource
+      }) : [];
+
+      // https://stackoverflow.com/questions/23450534/how-to-call-a-python-function-from-node-js
+       const spawn = require("child_process").spawn;
+       const pythonProcess = spawn('python',["model.py", JSON.stringify(patient_list)]); // Successfully parses JSON objects as string for use as Python argument
+       /* Python process takes as input a string which is a list of Patient FHIR resource objects.
+       Next steps:
+       1. Python needs to be parse string into JSON resource objects
+       2. Second argument needs to be given to Python of which patient is being compared to
+       3. Python needs to return only the FHIR JSONS of the top 5 most similar patients
+       4. Front end needs ability to select patient for similar search
+       5. JS needs to parse python output into readable patient entries for front-end
+       6. Front-end needs to display patients ranked by similarity next to patient compared to
+            a. Highlight the similar features (future feature)
+       */
+       pythonProcess.stdout.on('data', (data) => {
+         console.log(data.toString());
+       });
+
+
+      res.status(200).json(patients);
+    });
+  } else {
+    res.status(200).json([]);
+  }
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express server started');
+});
